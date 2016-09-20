d34c390ab534fef325035360b6a38afed8f9d642
Edu Garcia <edugar91@gmail.com>
2016-09-20 15:12:11 +0200

    Changed webdriver to chrome

--- firstTest.py
+++ firstTest.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("http://www.google.es")
 assert "Google" in driver.title
 driver.close()
