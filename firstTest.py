9a8ff65eed98514e879c485b9a18f129dbc85497
Edu Garcia <edugar91@gmail.com>
2016-09-20 15:11:24 +0200

    Added first test case

---
+++ firstTest.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.google.es")
+assert "Google" in driver.title
+driver.close()
